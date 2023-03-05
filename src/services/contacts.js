fe4d6f82a9eb186a6dd94d6c66e95a8f36439a2a
Marat Baikin <3371335@gmail.com>
2023-03-06 00:05:13 +0200

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,1 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
+
+const contactsInstance = axios.create({
+  baseURL: 'https://6404e2e0eed195a99f78132a.mockapi.io/api/contacts',
+});
+
+export const fetchContacts = async () => {
+  const { data } = await contactsInstance.get('/');
+  return data;
+};
