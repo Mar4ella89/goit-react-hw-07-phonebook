766f44e2d50a27aa8b95bb052d0e30f9c12027dc
Marat Baikin <3371335@gmail.com>
2023-03-06 15:10:51 +0200

    Update contact-slice.js

--- src/redux/contacts/contact-slice.js
+++ src/redux/contacts/contact-slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import { nanoid } from 'nanoid';
 
 import * as actions from './contact-actions';
 
@@ -12,59 +11,43 @@ const initialState = {
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
-  extraReducers: {
-    [actions.fetchAllContactsLoading]: store => {
-      store.loading = true;
-    },
-    [actions.fetchAllContactsSuccess]: (store, { payload }) => {
-      store.loading = false;
-      store.items = payload;
-    },
-    [actions.fetchAllContactsError]: (store, { payload }) => {
-      store.loading = false;
-      store.error = payload;
-    },
-    [actions.fetchAddContactLoading]: store => {
-      store.loading = true;
-    },
-    [actions.fetchAddContactSuccess]: (store, { payload }) => {
-      store.loading = false;
-      store.items.push(payload);
-    },
-    [actions.fetchAddContactError]: (store, { payload }) => {
-      store.loading = false;
-      store.error = payload;
-    },
-    [actions.fetchDeleteContactLoading]: store => {
-      store.loading = true;
-    },
-    [actions.fetchDeleteContactSuccess]: (store, { payload }) => {
-      store.loading = false;
-      const index = store.items.findIndex(item => item.id === payload);
-      store.items.splice(index, 1);
-    },
-    [actions.fetchDeleteContactError]: (store, { payload }) => {
-      store.loading = false;
-      store.error = payload;
-    },
-    // addContact: {
-    //   reducer: (state, { payload }) => {
-    //     state.push(payload);
-    //   },
-    //   prepare: ({ name, number }) => {
-    //     return {
-    //       payload: {
-    //         id: nanoid(),
-    //         name,
-    //         number,
-    //       },
-    //     };
-    //   },
-    // },
-    // deleteContact: (state, { payload }) =>
-    //   state.filter(item => item.id !== payload),
+  extraReducers: builder => {
+    builder
+      .addCase(actions.fetchAllContactsLoading, store => {
+        store.loading = true;
+      })
+      .addCase(actions.fetchAllContactsSuccess, (store, { payload }) => {
+        store.loading = false;
+        store.items = payload;
+      })
+      .addCase(actions.fetchAllContactsError, (store, { payload }) => {
+        store.loading = false;
+        store.error = payload;
+      })
+      .addCase(actions.fetchAddContactLoading, store => {
+        store.loading = true;
+      })
+      .addCase(actions.fetchAddContactSuccess, (store, { payload }) => {
+        store.loading = false;
+        store.items.push(payload);
+      })
+      .addCase(actions.fetchAddContactError, (store, { payload }) => {
+        store.loading = false;
+        store.error = payload;
+      })
+      .addCase(actions.fetchDeleteContactLoading, store => {
+        store.loading = true;
+      })
+      .addCase(actions.fetchDeleteContactSuccess, (store, { payload }) => {
+        store.loading = false;
+        const index = store.items.findIndex(item => item.id === payload);
+        store.items.splice(index, 1);
+      })
+      .addCase(actions.fetchDeleteContactError, (store, { payload }) => {
+        store.loading = false;
+        store.error = payload;
+      });
   },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
 export default contactsSlice.reducer;
